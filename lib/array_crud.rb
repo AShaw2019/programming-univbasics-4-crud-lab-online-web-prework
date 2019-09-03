54faba043e3cc165ebb43687b3b3527a81fc7b69
Ashley Twomey <aprier091012@gmail.com>
2019-09-03 14:43:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,15 +11,15 @@ def add_element_to_end_of_array("I", "am", "actually", "learning")
 end
 
 def add_element_to_start_of_array("I", "am", "actually", "learning", "arrays!")
-  
+  add_element_to_start_of_array.unshift("arrays!")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array("I", "am", "actually", "learning", "arrays!")
+  remove_element_from_end_of_array.pop("arrays!")
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array("I", "am", "actually", "learning", "arrays!")
+  remove_element_from_start_of_array.shift("arrays!")
 end
 
 def retrieve_element_from_index(array, index_number)
